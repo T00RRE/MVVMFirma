906b52ccaa5d565bdd895128bf6dff554b5a3686
T00RRE <mikolajleski5@gmail.com>
2024-12-21 12:06:42 +0100

    20.12.2024 dziala

---
+++ MVVMFirma/Models/EntitiesForView/ReklamacjaForAllView.cs
@@ -0,0 +1,14 @@
+﻿namespace MVVMFirma.Models.EntitiesForView
+{
+    public class ReklamacjaForAllView
+    {
+        public int IdReklamacji { get; set; }
+        public string Status { get; set; }
+        public System.DateTime? DataReklamacji { get; set; }
+        public string OpisReklamacji { get; set; }
+        
+        public string PracownikImie { get; set; }
+        public string PracownikNazwisko { get; set; }
+        public string Decyzja { get; set; }
+    }
+}
--- MVVMFirma/ViewModels/WszystkieReklamacjeViewModel.cs
+++ MVVMFirma/ViewModels/WszystkieReklamacjeViewModel.cs
@@ -1,11 +1,12 @@
 ﻿using MVVMFirma.Helper;
 using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace MVVMFirma.ViewModels
 {
-    public class WszystkieReklamacjeViewModel : WszystkieViewModel<Reklamacja>
+    public class WszystkieReklamacjeViewModel : WszystkieViewModel<ReklamacjaForAllView>
     {
         #region Fields
         private readonly Faktury2024Entities fakturyEntities;
@@ -22,7 +23,19 @@ namespace MVVMFirma.ViewModels
         #region Helpers
         public override void Load()
         {
-            List = new ObservableCollection<Reklamacja>(fakturyEntities.Reklamacja.ToList());
+            List = new ObservableCollection<ReklamacjaForAllView>(
+                from reklamacja in fakturyEntities.Reklamacja
+                select new ReklamacjaForAllView
+                {
+                    IdReklamacji = reklamacja.IdReklamacji,
+                    Status = reklamacja.Status,
+                    DataReklamacji = reklamacja.DataReklamacji,
+                    OpisReklamacji = reklamacja.OpisReklamacji,
+                    PracownikImie = reklamacja.Pracownik.Imie,
+                    PracownikNazwisko = reklamacja.Pracownik.Nazwisko,
+                    Decyzja = reklamacja.Decyzja
+                }
+            );
         }
         #endregion
     }
